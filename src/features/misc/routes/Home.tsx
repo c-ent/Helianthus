e7e820eaa79fa1cb541694e3670c99ebe30d5430
Elher Encarnacion <encarnacionelher@gmail.com>
2024-03-23 23:09:14 +0800

    created 1st section

--- src/features/misc/routes/Home.tsx
+++ src/features/misc/routes/Home.tsx
@@ -1,7 +1,21 @@
-
+import sunflowers from '../../../assets/images/sunflowers.png'
 
 export const Home = () => {
   return (
-    <div>home</div>
+    <div className="max-w-screen-xl m-auto p-5">
+      <section className='h-screen'>
+        <h1 className="font-serif text-[#FFA800] text-[250px] md: text-center  underline decoration-[3px] underline-offset-[50px]">Helianthus</h1>
+
+        <img src={sunflowers} alt="Sunflowers" className="w-[100%] h-[400px] object-cover object-center " />
+
+        <p className='text-[64px] mt-20 font-regular text-[#383838]'>
+        Helianti a Vincentio van Gogh mirabile testimonium ingenii eius manet, pulchritudinem atque vitalitatem naturae aeternam capiens.
+        </p>
+      </section>
+
+      <section>
+
+      </section>
+    </div>
   )
 }
