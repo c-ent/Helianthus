ae81dbbeb3424429b1f7bc52c8b9df325b7832f3
Elher Encarnacion <encarnacionelher@gmail.com>
2024-03-25 17:49:17 +0800

    minor

--- src/features/misc/routes/Home.tsx
+++ src/features/misc/routes/Home.tsx
@@ -2,9 +2,9 @@ import sunflowers from '../../../assets/images/sunflowers.png'
 
 export const Home = () => {
   return (
-    <div className="max-w-screen-xl m-auto p-5">
-      <section className='h-screen'>
-        <h1 className="font-serif text-[#FFA800] text-[250px] md: text-center  underline decoration-[3px] underline-offset-[50px]">Helianthus</h1>
+    <div className="max-w-screen-2xl m-auto p-5">
+      <section className='h-screen mt-20'>
+        <h1 className="font-Moglan text-[#FFA800] text-[100vw] md: text-center  underline decoration-[3px] underline-offset-[50px] leading-none">Helianthus</h1>
 
         <img src={sunflowers} alt="Sunflowers" className="w-[100%] h-[400px] object-cover object-center " />
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Moglan': ['Moglan', 'sans-serif']
+      }
+    }
   },
   plugins: [],
 }
